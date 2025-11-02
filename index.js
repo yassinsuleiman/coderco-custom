bbe96f39486854e57c18118a29388d0acea197b6
Yassin Suleiman <yassine.suleiman@proton.me>
2025-11-02 17:05:28 +0100

    create first custom action

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('actions/core')
+
+try {
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello', ${nameToGreet}!`);
+    core.setOutput('time', new Date().toTimeString());
+    
+
+    
+} catch (error) {
+        core.setFailed(error.message);
+    }
